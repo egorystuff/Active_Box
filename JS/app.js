2bf777550086f6f69661ccf1c654e2792cbd6959
Yahor Yankavets <mr.yankovec@gmail.com>
2023-02-03 14:56:47 +0300

    Added  JS slider for this project

--- JS/app.js
+++ JS/app.js
@@ -53,5 +53,17 @@ $(function () {
     nav.toggleClass('show');
   });
 
+  // reviews https://kenwheeler.github.io/slick/
+
+  let slider = $('#reviewsSlider');
+
+  slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: false,
+    dots: true,
+  });
   // console.log(elementOffset);
 });
